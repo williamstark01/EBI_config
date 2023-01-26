c4a8c995cce57858587beb0422fe95c53896aa5e
William Stark <william@ebi.ac.uk>
2023-01-26 15:04:28 +0000

    add function to install ML packages

--- create_ML_Jupyter_project.sh
+++ create_ML_Jupyter_project.sh
@@ -57,4 +57,19 @@ create_Python_project() {
 }
 
 
-create_project
+install_ml_packages() {
+    echo "Installing Machine Learning packages..."
+
+    poetry add biopython jupyterlab matplotlib numpy pandas pytorch-lightning scikit-learn tensorflow torch
+
+    echo
+}
+
+
+create_Python_project
+
+# NOTE
+# There is an issue with automating the `poetry add <...>` command, see
+# https://github.com/python-poetry/poetry/issues/651
+# https://python-poetry.org/docs/configuration/#virtualenvsprefer-active-python-experimental
+#install_ml_packages
