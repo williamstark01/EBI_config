1dd14559f332969060e9816fa5b1e5c588914a6b
William Stark <william@ebi.ac.uk>
2023-01-26 15:08:10 +0000

    remove code duplication

--- create_ML_Jupyter_project.sh
+++ create_ML_Jupyter_project.sh
@@ -8,53 +8,9 @@
 set -e
 
 
-SCRIPT_DIRECTORY="$(dirname "$(readlink -f "$0")")"
-
-
-create_Python_project() {
-    if ! command -v pyenv &> /dev/null ; then
-        echo "Couldn't run pyenv, check it is installed and properly configured."
-    fi
-    if [[ -z "$PYENV_ROOT" ]]; then
-        echo "The PYENV_ROOT environment variable is not set, check that pyenv is installed and properly configured."
-    fi
-
-    read -p "How should the project be called? " PROJECT_NAME
-    echo
-
-    mkdir $PROJECT_NAME
-    cd $PROJECT_NAME
-
-    echo "Creating a git repository for the project..."
-    git init
-    git checkout -b main
-
-    cp "$SCRIPT_DIRECTORY/.gitignore" .
-    git add .gitignore
-    git commit -m "add .gitignore"
-    echo
-
-    echo "Creating a dedicated Python virtual environment for the project $PROJECT_NAME..."
-    pyenv virtualenv "$PROJECT_NAME"
-    pyenv local "$PROJECT_NAME"
-    echo "Upgrading the pip module..."
-    pip install --upgrade pip
-    echo
-
-    git add .python-version
-    git commit -m "specify virtual environment"
-
-    echo "Initializing the project with Poetry..."
-    poetry init --no-interaction --quiet --name $PROJECT_NAME
-    echo
-
-    git add pyproject.toml
-    git commit -m "create Python project"
-    echo
-
-    echo "The project has been created, using Python version $(pyenv global)"
-    echo "cd to the $PROJECT_NAME directory and start hacking!"
-}
+# create_Python_project() {
+# <...>
+# }
 
 
 install_ml_packages() {
@@ -65,8 +21,6 @@ install_ml_packages() {
     echo
 }
 
-
-create_Python_project
 
 # NOTE
 # There is an issue with automating the `poetry add <...>` command, see
