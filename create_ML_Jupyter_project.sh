f473be062661770e80c09f9de70239bc6b6dbc78
William Stark <william@ebi.ac.uk>
2023-01-27 16:20:39 +0000

    update for repurposing the script

--- create_ML_Jupyter_project.sh
+++ create_ML_Jupyter_project.sh
@@ -8,22 +8,22 @@
 set -e
 
 
-# create_Python_project() {
-# <...>
-# }
+#create_Python_project() {
+#    # identical function to the hononumous one in the create_Python_project.sh script
+#<...>
+#}
 
 
 install_ml_packages() {
+    # NOTE
+    # There is an issue with automating the `poetry add <...>` command, see
+    # https://github.com/python-poetry/poetry/issues/651
+    # https://python-poetry.org/docs/configuration/#virtualenvsprefer-active-python-experimental
+
     echo "Installing Machine Learning packages..."
 
     poetry add biopython jupyterlab matplotlib numpy pandas pytorch-lightning scikit-learn tensorflow torch
 
     echo
 }
-
-
-# NOTE
-# There is an issue with automating the `poetry add <...>` command, see
-# https://github.com/python-poetry/poetry/issues/651
-# https://python-poetry.org/docs/configuration/#virtualenvsprefer-active-python-experimental
 #install_ml_packages
