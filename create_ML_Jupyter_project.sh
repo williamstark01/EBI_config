cb715dc75d078fb5105d6b691a58a77ca5c9ae56
William Stark <william@ebi.ac.uk>
2023-01-27 19:05:59 +0000

    WIP mostly complete

--- create_ML_Jupyter_project.sh
+++ create_ML_Jupyter_project.sh
@@ -8,10 +8,14 @@
 set -e
 
 
+SCRIPT_DIRECTORY="$(dirname "$(readlink -f "$0")")"
+
+
 #create_Python_project() {
-#    # identical function to the hononumous one in the create_Python_project.sh script
+#    # function identical to the hononumous one in the create_Python_project.sh script
 #<...>
 #}
+#create_Python_project
 
 
 install_ml_packages() {
@@ -27,3 +31,98 @@ install_ml_packages() {
     echo
 }
 #install_ml_packages
+
+
+create_ML_Jupyter_project() {
+    read -p "How should the project be called? " PROJECT_NAME
+    echo
+
+
+    # create project directory and git repository
+    ############################################################################
+    mkdir $PROJECT_NAME
+    cd $PROJECT_NAME
+
+    echo "Creating a git repository for the project..."
+    git init
+    git checkout -b main
+
+    cp "$SCRIPT_DIRECTORY/.gitignore" .
+    git add .gitignore
+    git commit -m "add .gitignore"
+    echo
+    ############################################################################
+
+
+    # clone ML project template virtual environment
+    ############################################################################
+    echo "Cloning ML project template virtual environment... (Takes a few minutes.)"
+
+    PYENV_VERSIONS_DIRECTORY="$(pyenv root)/versions"
+
+    # # create directory structure for the new virtual environment
+    # VENV_PARENT_DIRECTORY="$PYENV_VERSIONS_DIRECTORY/3.10.9/envs"
+    # mkdir --parents --verbose "$VENV_PARENT_DIRECTORY"
+
+    #VENV_DIRECTORY="$VENV_PARENT_DIRECTORY/$PROJECT_NAME"
+    VENV_DIRECTORY="$PYENV_VERSIONS_DIRECTORY/$PROJECT_NAME"
+
+    GB_ML_PROJECT_TEMPLATE_VENV_DIRECTORY="/nfs/production/flicek/ensembl/genebuild/william/.pyenv/versions/3.10.9/envs/gb_ml_project_template"
+    cp --interactive --preserve --recursive "$GB_ML_PROJECT_TEMPLATE_VENV_DIRECTORY" "$VENV_DIRECTORY"
+    #ln --symbolic "$VENV_DIRECTORY" "$PYENV_VERSIONS_DIRECTORY"
+
+    pyenv local "$PROJECT_NAME"
+    echo
+
+    git add .python-version
+    git commit -m "specify virtual environment"
+    echo
+    ############################################################################
+
+
+    # clone Poetry pyproject.toml template configuration
+    ############################################################################
+    echo "Cloning Poetry pyproject.toml template configuration..."
+
+    PYPROJECT_TOML_TEMPLATE="/nfs/production/flicek/ensembl/genebuild/william/ML_Jupyter/gb_ml_project_template/pyproject.toml"
+
+    cp --interactive --preserve "$PYPROJECT_TOML_TEMPLATE" .
+
+    git add pyproject.toml
+    git commit -m "import pyproject.toml project configuration file"
+    echo
+    ############################################################################
+
+
+    # create a tmux session and start the JupyterLab server
+    ############################################################################
+    TMUX_SESSION_NAME=$PROJECT_NAME
+
+    # create a detached tmux session
+    tmux new-session -d -s "$TMUX_SESSION_NAME" -n "jupyterlab"
+
+    # run a JupyterLab server bsub job in the tmux session
+    MEM_LIMIT=16384
+    PORT=54321
+    JUPYTERLAB_BSUB_JOB="bsub -q production -M $MEM_LIMIT -Is -tty jupyter-lab --port=$PORT --no-browser --ip 0.0.0.0"
+    tmux send-keys -t "${TMUX_SESSION_NAME}:jupyterlab" "$JUPYTERLAB_BSUB_JOB" ENTER
+
+    # connect to the tmux session
+    tmux attach-session -t $TMUX_SESSION_NAME
+
+    # TODO
+    # echo the following if the above fails
+    #echo "attach to the annotation tmux session with:"
+    #echo "tmux attach-session -t $tmux_session_name"
+
+    # open JupyterLab with the provided URL over VPN / SSH
+    # e.g.
+    # http://hl-codon-14-04.ebi.ac.uk:54321/lab
+    ############################################################################
+
+    # TODO
+    # echo successful completion message?
+}
+
+
+create_ML_Jupyter_project
